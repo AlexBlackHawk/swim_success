3857541a0fab518682302a4c680f2575bc27cedc
Alex Maudza <71087168+AlexBlackHawk@users.noreply.github.com>
2026-06-18 00:09:12 +0300

    refactor: change file structure (#5)
    In this commit file structure for response models were changed. Also class name for user model was renamed. For pace seconds request model directory, file and class were renamed.

---
+++ lib/data/models/request_models/pace_seconds_model/pace_seconds_model.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'pace_seconds_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+PaceSecondsModel _$PaceSecondsModelFromJson(Map<String, dynamic> json) =>
+    PaceSecondsModel(paceSeconds: json['pace_seconds'] as String);
+
+Map<String, dynamic> _$PaceSecondsModelToJson(PaceSecondsModel instance) =>
+    <String, dynamic>{'pace_seconds': instance.paceSeconds};
