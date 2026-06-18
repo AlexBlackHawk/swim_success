4c26e7ac2cbc4e4b20d08a7a644b0e2d24d2aa41
Alex Maudza <71087168+AlexBlackHawk@users.noreply.github.com>
2026-06-18 22:11:18 +0300

    feat: add routing (#10)
    In this commit routing was added using GoRouter.

---
+++ lib/presentation/routing/app_router.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:swim_success/presentation/routing/enums/app_page.dart';
+
+List<RouteBase> buildAppRoutes() => [
+  GoRoute(
+    path: setTimePageRoute.path,
+    name: setTimePageRoute.name,
+    builder: (context, state) => SetTimePage(),
+  ),
+  GoRoute(
+    path: usersListPageRoute.path,
+    name: usersListPageRoute.name,
+    builder: (context, state) => UsersListPage(),
+  ),
+  GoRoute(
+    path: userDetailsPageRoute.path,
+    name: userDetailsPageRoute.name,
+    builder: (context, state) => UserDetailsPage(),
+  ),
+];
