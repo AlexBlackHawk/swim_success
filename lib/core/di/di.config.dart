e822a296189514940841b9b656aa98932af04db6
AlexBlackHawk <alex.maudza@gmail.com>
2026-06-18 12:25:25 +0300

    feat: add requests
    In this commit dio requests for sending pace seconds and getting users were added. Also dio client was created.

--- lib/core/di/di.config.dart
+++ lib/core/di/di.config.dart
@@ -9,8 +9,12 @@
 // coverage:ignore-file
 
 // ignore_for_file: no_leading_underscores_for_library_prefixes
+import 'package:dio/dio.dart' as _i361;
 import 'package:get_it/get_it.dart' as _i174;
 import 'package:injectable/injectable.dart' as _i526;
+import 'package:swim_success/core/modules/app_module/app_module.dart' as _i34;
+import 'package:swim_success/data/api/posts_api/posts_api.dart' as _i147;
+import 'package:swim_success/data/api/users_api/users_api.dart' as _i297;
 
 // initializes the registration of main-scope dependencies inside of GetIt
 _i174.GetIt $configureDependencies(
@@ -18,6 +22,12 @@ _i174.GetIt $configureDependencies(
   String? environment,
   _i526.EnvironmentFilter? environmentFilter,
 }) {
-  _i526.GetItHelper(getIt, environment, environmentFilter);
+  final gh = _i526.GetItHelper(getIt, environment, environmentFilter);
+  final appModule = _$AppModule();
+  gh.lazySingleton<_i361.Dio>(() => appModule.dio);
+  gh.lazySingleton<_i147.PostsApi>(() => appModule.postsApi(gh<_i361.Dio>()));
+  gh.lazySingleton<_i297.UsersApi>(() => appModule.usersApi(gh<_i361.Dio>()));
   return getIt;
 }
+
+class _$AppModule extends _i34.AppModule {}
--- lib/core/modules/app_module/app_module.dart
+++ lib/core/modules/app_module/app_module.dart
@@ -1,6 +1,17 @@
+import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
+import 'package:swim_success/core/modules/app_module/create_dio_client.dart';
+import 'package:swim_success/data/api/posts_api/posts_api.dart';
+import 'package:swim_success/data/api/users_api/users_api.dart';
 
 @module
 abstract class AppModule {
+  @lazySingleton
+  Dio get dio => createDioClient();
 
+  @lazySingleton
+  PostsApi postsApi(Dio dio) => PostsApi(dio);
+
+  @lazySingleton
+  UsersApi usersApi(Dio dio) => UsersApi(dio);
 }
